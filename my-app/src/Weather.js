9e4859e2c5e8f5f8ceaee53fcbed1212727027f8
Alexander Hristoskov <hristoskov.alexander@gmail.com>
2018-02-19 18:49:25 +0200

    Adjusting styles

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -56,12 +56,16 @@ export default class Weather extends React.Component{
         {
           elements.push(
             <div className="col-sm">
-              <h3>{item.date.weekday}</h3> 
-              <p><img src={item.icon_url} alt={item.icon} /></p>          
-              <p>Weather - {item.conditions}</p>
-              <p>Temp high - {item.high.celsius} C&deg; </p>
-              <p>Temp low - {item.low.celsius} C&deg; </p>
-              <p>Wind - {item.avewind.kph} K/PH</p>               
+              <div className="card">
+                <div className="card-body">            
+                  <h4 className="card-title">{item.date.weekday}</h4>                 
+                  <p><img src={item.icon_url} alt={item.icon} /></p>          
+                  <p>Weather - {item.conditions}</p>
+                  <p>Temp high - {item.high.celsius} C&deg; </p>
+                  <p>Temp low - {item.low.celsius} C&deg; </p>
+                  <p>Wind - {item.avewind.kph} K/PH</p>    
+                </div>
+              </div>                         
             </div>
           )
         });
@@ -69,17 +73,20 @@ export default class Weather extends React.Component{
       return(
         <div className="row"> 
           <div className="col-12">  
-            <h1> Weather - {this.state.city.full} </h1>                                                       
-          </div>                              
-          <hr />
+            <h2> Weather - {this.state.city.full} </h2>                    
+          </div>                                        
           <div className="w-100 margin-top-10"></div>
           <div className="col-sm">
-            <h3>Today</h3>
-            <p><img src={this.state.dataToday.icon_url} alt={this.state.dataToday.icon} /></p>            
-            <p>Weather - {this.state.dataToday.weather}</p>
-            <p>Temp - {this.state.dataToday.temp_c} C&deg; </p>
-            <p>Wind - {this.state.dataToday.wind_kph} K/PH</p>
-            <p>Updated @ {this.state.dataToday.local_time_rfc822}</p>                  
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Current</h4>                 
+                <p><img src={this.state.dataToday.icon_url} alt={this.state.dataToday.icon} /></p>            
+                <p>Weather - {this.state.dataToday.weather}</p>
+                <p>Temp - {this.state.dataToday.temp_c} C&deg; </p>
+                <p>Wind - {this.state.dataToday.wind_kph} K/PH</p>
+                <p>Updated @ {this.state.dataToday.local_time_rfc822}</p>                  
+              </div>
+            </div>
           </div>
           {elements}
         </div>
