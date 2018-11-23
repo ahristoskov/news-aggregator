6b98956267791f40399c72c4b9db141a69102627
Alexander Hristoskov <hristoskov.alexander@gmail.com>
2018-11-23 13:24:00 +0200

    adding build folder

---
+++ my-app/build/precache-manifest.37ebebcf3139bc0416bd1f3ed247e66f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11bd3611dce6f2e4cbc1",
+    "url": "/static/js/main.11bd3611.chunk.js"
+  },
+  {
+    "revision": "4166ae5d8417e1d1d7ef",
+    "url": "/static/js/1.4166ae5d.chunk.js"
+  },
+  {
+    "revision": "11bd3611dce6f2e4cbc1",
+    "url": "/static/css/main.c69a5a45.chunk.css"
+  },
+  {
+    "revision": "581ece5668afdad37eecc0e572a2d3a4",
+    "url": "/index.html"
+  }
+];
